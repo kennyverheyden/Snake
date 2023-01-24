bb58b1821ae51bcf4507ffd90f0916653d4a1ff8
Kenny Verheyden <54863392+kennyverheyden@users.noreply.github.com>
2023-01-24 23:17:27 +0100

    Snake + Candy

--- src/main/ControllerMain.java
+++ src/main/ControllerMain.java
@@ -1,14 +1,18 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.Optional;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
@@ -36,8 +40,18 @@ public class ControllerMain {
 	@FXML
 	private Rectangle rectangle;
 
-	private int numberOfRows=15;
-	private int numberOfColumns=15;
+	//Parameters
+	private int numberOfRows=15; 	// Board size rows
+	private int numberOfColumns=15; // Board size columns
+	private int snakeLength=3; // Snake length
+	private int y=4;
+	private int x=0;
+	private String setDirection="right";
+
+	private Random random= new Random();
+	int rdX,rdY;
+
+	private ArrayList<Node> snake = new ArrayList<Node>();
 
 	private Rectangle[][] boardElements = null;
 
@@ -45,45 +59,152 @@ public class ControllerMain {
 	{
 		fillBoard();
 		initializeGridPaneArray();
-		Rectangle rctRectangle = (Rectangle) boardElements[0][0];
-		rctRectangle.setFill(Color.DARKKHAKI);
+		setSnake();
+		setCandy();
+		controls();
+		run();
+	}
+
+	// Run the snake
+	private void run() {
 
 		Timer timer = new Timer();
 		TimerTask task = new TimerTask() { // with anonymous class
-			int x=0;
-			int y=0;
 
 			@Override
 			public void run()
 			{
-				if (x == numberOfColumns) {
-					boardElements[x-1][y].setFill(Color.web("#ffe494"));
-					x = 0;
-				} else if (x < 0) {
-					x = numberOfColumns - 1;
-				}
-
-				if (y >= numberOfRows) {
-					boardElements[x][y-1].setFill(Color.web("#ffe494"));
-					y = 0;
-				} else if (y < 0) {
-					y = numberOfRows - 1;
-				}
-				if(x!=0)
-				{
-					boardElements[x-1][y].setFill(Color.web("#ffe494"));
-				}
-				if(y!=0)
-				{
-					boardElements[x][y-1].setFill(Color.web("#ffe494"));
-				}
-				boardElements[x][y].setFill(Color.DARKKHAKI);
-				y++;
+				moveSnake();
 			}
 		};
-		timer.schedule(task, 2000,500);
+		timer.schedule(task, 1000,400);
 	}
 
+	// Set candy
+	private void setCandy() {
+		rdX = random.nextInt(numberOfRows-1);
+		rdY = random.nextInt(numberOfColumns-1);
+		Rectangle rectCandy= (Rectangle)boardElements[rdX][rdY];
+		rectCandy.setFill(Color.GREEN);
+	}
+
+	// Set snake start position
+	private void setSnake() {
+
+		for (int i = 0; i < snakeLength; i++) {
+			Rectangle rect= (Rectangle) boardElements[i][y];
+			x=i;
+			AddSnakeBlocks(rect);
+			x++;
+		}
+	}
+
+	private void AddSnakeBlocks(Rectangle rect) {
+		((Shape) rect).setFill(Color.DARKKHAKI);
+		snake.add(rect);
+		if (snake.size()== snakeLength+1) {
+			removeSnakeBlocks();
+		}
+
+	}
+
+	private void removeSnakeBlocks() {
+		Rectangle rect2= (Rectangle)snake.get(0);
+		rect2.setFill(Color.web("#ffe494"));
+		snake.remove(0);
+	}
+
+	public void moveSnake() {
+		nextPosition();
+	}
+
+	private void nextPosition() {
+
+		// Got the candy
+		if(x== rdX && y== rdY) {
+			snakeLength++;
+			setCandy();
+		}
+
+		AddSnakeBlocks(boardElements[x][y]);
+
+		if(setDirection=="left")
+		{
+			if(x==0)
+			{
+				x=numberOfColumns-1;
+			}
+			else
+			{
+				x--;
+			}
+		}
+		if(setDirection=="right")
+		{
+			if(x==numberOfColumns-1)
+			{
+				x=0;
+			}
+			else
+			{
+				x++;
+			}
+		}
+		if(setDirection=="down")
+		{
+			if(y==numberOfRows-1)
+			{
+				y=0;
+			}
+			else
+			{
+				y++;
+			}
+		}
+		if(setDirection=="up")
+		{
+			if(y==0)
+			{
+				y=numberOfRows-1;
+			}
+			else
+			{
+				y--;
+			}
+		}
+	}
+
+	// Controls
+	private void controls() {
+		// Snake Controls
+		rootPane.addEventFilter(KeyEvent.KEY_PRESSED, new EventHandler<KeyEvent>() {
+
+			@Override
+			public void handle(KeyEvent event) {
+
+				switch (event.getCode()) {
+				case UP:
+					setDirection="up";
+					break;
+				case DOWN:
+					setDirection="down";
+					break;
+				case LEFT:
+					setDirection="left";
+					break;
+				case RIGHT:
+					setDirection="right";
+					break;
+				default:
+					break;
+				}
+			}
+
+		});
+
+	}
+
+	// Init board array
 	public void initializeGridPaneArray()
 	{
 		boardElements = new Rectangle[numberOfRows][numberOfColumns];
@@ -93,6 +214,7 @@ public class ControllerMain {
 		}
 	}
 
+	// Fill the board
 	private void fillBoard() {
 		int numberOfRows = 15;
 		int numberOfColumns=15;
@@ -107,6 +229,7 @@ public class ControllerMain {
 		}
 	}
 
+	// Exit the game
 	@FXML
 	void onClickbtnExit(ActionEvent event) {
 		// Box ask confirmation
@@ -122,6 +245,11 @@ public class ControllerMain {
 			// Exit the program
 			System.exit(0);
 		}
+	}
+
+	// Enum Directions
+	public enum Direction {
+		UP, DOWN, LEFT, RIGHT
 	}
 
 }
